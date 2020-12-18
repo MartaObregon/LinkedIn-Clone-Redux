4008ca93e796296c454c4bae78730b89b99c6014
Marta Obregon <marta.garcia.obregon@.com>
2020-12-18 17:42:23 +0000

    setup & header

---
+++ src/Header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import SearchIcon from '@material-ui/icons/Search';
+import './Header.css'
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
+import ChatIcon from '@material-ui/icons/Chat'
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+function Header() {
+    return (
+        <div className="header">
+            
+            <div className="header_left">
+                <img alt ="" src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"/>
+
+
+                <div className="header_search">
+
+                    <SearchIcon/>
+                    <input type="text"/>
+
+                </div>
+
+            </div>
+
+            <div className="header_right">
+
+            <HeaderOption title="Home" Icon={HomeIcon}/>
+            <HeaderOption title="My Network" Icon={SupervisorAccountIcon}/>
+            <HeaderOption title="Messaging" Icon={ChatIcon}/>
+            <HeaderOption title="Notifications" Icon={NotificationsIcon}/>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
